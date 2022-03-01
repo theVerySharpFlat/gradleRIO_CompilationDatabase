f9a073cb6d5377552a043b9681e57d9ad644b616
theVerySharpFlat <aidenramen29@gmail.com>
2022-03-01 03:42:32 +0000

    add cliargs

--- rioCDGen.py
+++ rioCDGen.py
@@ -1,14 +1,47 @@
 #!/bin/python3
-import os, sys
+import os
+import sys
 import datetime
 import json
+import argparse
 
-year = datetime.date.today().year
-buildType = "Debug"
+# parse arguments
+argParser = argparse.ArgumentParser(
+    description="Generate a compile_commands.json for an frc gradle project")
+argParser.add_argument("--compiler", type=str,
+                       help="path to the cross-compiler that gradleRIO uses. If not specified, the script will search for it")
+argParser.add_argument("--build-type", "-bt",
+                       choices=["Debug", "Release"], default="Debug", help="The type of build. Can be Debug or Release. Defaults to Debug")
+argParser.add_argument("--year", "-y", type=int,
+                       help="The year of the toolchain/project. Defaults to the current year", default=datetime.date.today().year)
+argParser.add_argument("--source", "-S", "-s", type=str,
+                       help="The root directory of the project(should be the directory with your build.gradle file. Defaults to cwd", default=os.getcwd())
+args = argParser.parse_args()
 
+year = args.year
+buildType = args.build_type
+
+if not os.path.exists(args.source):
+    print("project directory does not exist!")
+    exit(1)
+elif not os.path.exists(os.path.join(args.source, "build.gradle")):
+    print("project directory does not contain a build.gradle! Is this a project directory?")
+    exit(1)
+else:
+    os.chdir(args.source)
 # finds the roboRIO g++ cross compiler
+
+
 def findCompiler() -> str:
-    name = os.path.sep + os.path.join(f"{year}", "roborio", "bin", f"arm-frc{year}-linux-gnueabi-g++")
+    if args.compiler:
+        if os.path.exists(args.compiler):
+            return args.compiler
+        else:
+            print(f"Compiler path does not exist! Looking elsewhere...")
+
+    name = os.path.sep + \
+        os.path.join(f"{year}", "roborio", "bin",
+                     f"arm-frc{year}-linux-gnueabi-g++")
     path = os.path.expanduser('~')
     for root, dirs, files in os.walk(path):
         for file in files:
@@ -16,23 +49,30 @@ def findCompiler() -> str:
                 return os.path.join(root, file)
 
 # find the options file
+
+
 def findOptionsTXT() -> str():
-    name = os.path.join(f"compileFrcUserProgram{buildType}ExecutableFrcUserProgramCpp", "options.txt")
+    name = os.path.join(
+        f"compileFrcUserProgram{buildType}ExecutableFrcUserProgramCpp", "options.txt")
     for root, dirs, files in os.walk(os.getcwd()):
         for file in files:
             if name in os.path.join(root, file):
                 return os.path.join(root, file)
 
 # get the options from the file (replace newlines with spaces)
+
+
 def getOptionString(filePath: str) -> str:
     optionString = None
     with open(filePath) as file:
         dat = file.read()
         optionsString = dat.replace("\n", " ")
-    
+
     return optionsString
 
 # find all cpp files
+
+
 def getAllCppFiles():
     foundFiles = []
     for root, dirs, files in os.walk(os.getcwd()):
@@ -42,10 +82,12 @@ def getAllCppFiles():
 
     return foundFiles
 
+
 def findFile(filename: str, filepath: str) -> str:
     for root, dirs, files in os.walk(filepath):
         if filename in files:
             return os.path.join(root, filename)
+
 
 def generateCompileCommands(compilerPath: str, optionString: str):
     model = []
@@ -54,7 +96,8 @@ def generateCompileCommands(compilerPath: str, optionString: str):
         objectFile = findFile(objectFile, os.path.join(os.getcwd(), "build"))
 
         if objectFile == None:
-            print(f"  failed to find object file for cpp file \"{file}\". Skipping...")
+            print(
+                f"  failed to find object file for cpp file \"{file}\". Skipping...")
             continue
         else:
             print(f"  generating definition for {file}")
@@ -70,6 +113,7 @@ def generateCompileCommands(compilerPath: str, optionString: str):
     print(f"dumping to {os.path.join(os.getcwd(), 'compile_commands.json')}")
     with open("compile_commands.json", mode='w') as compilationDatabaseFile:
         json.dump(model, compilationDatabaseFile)
+
 
 def main():
 
@@ -97,9 +141,10 @@ def main():
         exit(1)
     print("success")
 
-    #generate the file
+    # generate the file
     print("generating compile_commands.json... ")
     generateCompileCommands(compilerPath, compilerOptions)
+
 
 if __name__ == "__main__":
     main()
